65d99de6ff715bace3d077a30d4184e27ec3d180
Vishal Singh <vishalsinghbrn@gmail.com>
2026-01-31 14:28:08 +0530

    indexvalue

---
+++ Array/indexvalue.java
@@ -0,0 +1,10 @@
+public class indexvalue {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,8,7};
+       int num=arr.length;
+        for(int i=0;i<=num;i++){
+            System.out.print(i);
+        }
+
+    }
+}
