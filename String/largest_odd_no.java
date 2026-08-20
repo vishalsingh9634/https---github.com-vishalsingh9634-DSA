8be063a0e4bd3b34ba064e2f1fc49a33d9115fd7
Vishal Singh <vishalsinghbrn@gmail.com>
2026-08-20 23:32:49 +0530

    largest odd no

---
+++ String/largest_odd_no.java
@@ -0,0 +1,25 @@
+public class largest_odd_no {
+    public static String largest_odd(String s){
+        int end=s.length()-1;
+        while(end>0){
+            int digit=s.charAt(end)-'0';
+            if(digit%2==1){
+                break;
+            }
+            end--;
+            if(end<0){
+                return "";
+            }
+            
+        }
+        int start=0;
+            while(s.charAt(start)=='0'){
+                start++;
+            }
+            return s.substring(start,end+1);
+    }
+    public static void main(String[] args) {
+        String s ="0214638";
+        System.out.println(largest_odd(s));
+    }
+}
